85176a4ac5c0104cc79696d660697692f84bf18c
jvf350 <158082887+jvf350@users.noreply.github.com>
2024-01-29 19:03:11 +0100

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,334 @@
+import java.awt.BorderLayout;
+ 
+public class Calculadora {
+ 
+	// Constantes
+	final int MAX_DIGITS = 5;
+	final int MODE_ENTRADA = 0;
+	final int MODE_RESULTADO = 1;
+	 
+	//Variables
+	int modo;
+	int valor1;
+	int valor2;
+	String operacion;
+	boolean inicializa_resultado;
+	 
+	private static Texto texto_resultado;
+	 
+	public Calculadora(boolean gui) {
+	 
+		//Inicialización de las variables.
+		inicializa();
+		 
+		if (gui==true) dibujaCalculadora();
+		 
+	}
+	 
+	private void dibujaCalculadora() {
+	 
+		Display display = Display.getDefault();
+		Shell shlCalculadora = new Shell();
+		shlCalculadora.setSize(259, 250);
+		shlCalculadora.setText("Calculadora");
+		 
+		//------------------------------------------------ -
+		//Números
+		//------------------------------------------------ -
+		 
+		//botón con el número 0
+		Button button_0 = new Button(shlCalculadora, SWT.NONE);
+		button_0.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(0);
+		}
+		});
+		button_0.setText("0");
+		button_0.setBounds(23, 163, 40, 33);
+		 
+		//botón con el número 1
+		Button button_1 = new Button(shlCalculadora, SWT.NONE);
+		button_1.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(1);
+		}
+		});
+		button_1.setText("1");
+		button_1.setBounds(23, 124, 40, 33);
+		 
+		//botón con el número 2
+		Button button_2 = new Button(shlCalculadora, SWT.NONE);
+		button_2.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(2);
+		}
+		});
+		button_2.setText("2");
+		button_2.setBounds(69, 124, 40, 33);
+		 
+		//botón con el número 3
+		Button button_3 = new Button(shlCalculadora, SWT.NONE);
+		button_3.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(3);
+		}
+		});
+		button_3.setText("3");
+		button_3.setBounds(115, 124, 40, 33);
+		 
+		//botón con el número 4
+		Button button_4 = new Button(shlCalculadora, SWT.NONE);
+		button_4.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(4);
+		}
+		});
+		button_4.setText("4");
+		button_4.setBounds(23, 85, 40, 33);
+		 
+		//botón con el número 5
+		Button button_5 = new Button(shlCalculadora, SWT.NONE);
+		button_5.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(5);
+		}
+		});
+		button_5.setText("5");
+		button_5.setBounds(69, 85, 40, 33);
+		 
+		//botón con el número 6
+		Button button_6 = new Button(shlCalculadora, SWT.NONE);
+		button_6.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(6);
+		}
+		});
+		button_6.setText("6");
+		button_6.setBounds(115, 85, 40, 33);
+		 
+		//botón con el número 7
+		Button button_7 = new Button(shlCalculadora, SWT.NONE);
+		button_7.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(7);
+		}
+		});
+		button_7.setText("7");
+		button_7.setBounds(23, 46, 40, 33);
+		 
+		//botón con el número 8
+		Button button_8 = new Button(shlCalculadora, SWT.NONE);
+		button_8.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(8);
+		}
+		});
+		button_8.setBounds(69, 46, 40, 33);
+		button_8.setText("8");
+		 
+		//botón con el número 9
+		Button button_9 = new Button(shlCalculadora, SWT.NONE);
+		button_9.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			anadeNuevoDigito(9);
+		}
+		});
+		button_9.setText("9");
+		button_9.setBounds(115, 46, 40, 33);
+		 
+		//------------------------------------------------ -
+		//Operaciones
+		//------------------------------------------------ -
+		 
+		//botón con la operacion de división
+		Button button_12 = new Button(shlCalculadora, SWT.NONE);
+		button_12.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			ejecutarOperador("/");
+		}
+		});
+		button_12.setText("/");
+		button_12.setBounds(178, 46, 40, 33);
+		 
+		//botón con la operacion de multiplicación
+		Button button_13 = new Button(shlCalculadora, SWT.NONE);
+		button_13.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			ejecutarOperador("*");
+		}
+		});
+		button_13.setText("*");
+		button_13.setBounds(178, 85, 40, 33);
+		 
+		//botón con la operacion de suma
+		Button button_14 = new Button(shlCalculadora, SWT.NONE);
+		button_14.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			ejecutarOperador("+");
+		}
+		});
+		button_14.setText("+");
+		button_14.setBounds(178, 124, 40, 33);
+		 
+		//botón con la operacion de resto
+		Button button_15 = new Button(shlCalculadora, SWT.NONE);
+		button_15.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			ejecutarOperador("-");
+		}
+		});
+		button_15.setText("-");
+		button_15.setBounds(178, 163, 40, 33);
+		 
+		//botón con la operacion de igual
+		Button button_11 = new Button(shlCalculadora, SWT.NONE);
+		button_11.addSelectionListener(new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			ejecutarIgual();
+		}
+		});
+		button_11.setText("=");
+		button_11.setBounds(69, 163, 86, 33);
+		 
+		//Texto donde se visualiza el resultado
+		text_resultado = new Text(shlCalculadora, SWT.BORDER);
+		text_resultado.setText("0");
+		text_resultado.setBounds(22, 19, 196, 21);
+		 
+		shlCalculadora.open();
+		shlCalculadora.layout();
+		while (!shlCalculadora.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+	 
+	public void inicializa() {
+		operacion = "null";
+		valor1 = 0;
+		valor2 = 0;
+		modo = MODE_ENTRADA;
+		inicializa_resultado = true;
+	}
+	 
+	public String getResultadoString (){
+		return text_resultado.getText();
+	}
+	 
+	public void setResultatString(String s){
+		text_resultado.setText(s);
+	}
+	 
+	public int getResultadoInt() {
+		String resultado = text_resultado.getText();
+		return Integer.parseInt(resultado);
+	}
+	 
+	public void anadeNuevoDigito(int digit){
+		if (inicializa_resultado)
+			setResultadoString("");
+	 
+		String inputString = getResultadoString();
+	 
+		if (inputString.indexOf("0") == 0){
+			inputString = inputString.substring(1);
+		}
+	 
+		if ((!inputString.equals("0") || digito > 0) && inputString.length() < MAX_DIGITS){
+			setResultatString(inputString + digito);
+		}
+	 
+		modo = MODE_ENTRADA;
+		inicializa_resultado = false;
+	}
+	 
+	public void ejecutarOperador(String new_operacion) {
+	 
+		int resultado;
+	 
+		if (operacion.equals("null"))
+		{
+			resultado = getResultadoInt();
+			valor1 = resultado;
+		}
+	 
+		else
+		{
+			valor2 = getResultadoInt();
+			resultado = ejecutarOperacion();
+			muestraResultado(resultado);
+			valor1 = resultado;
+		}
+	 
+		inicializa_resultado = true;
+		operacion = new_operacion;
+	}
+	 
+	public void ejecutarIgual(){
+	int resultado = 0;
+	 
+	valor2 = getResultadoInt();
+	resultado = ejecutarOperacion();
+	muestraResultado(resultado);
+	 
+	operacion = "null";
+	}
+	 
+	public int ejecutarOperacion() {
+		int resultado = 0;
+	 
+		if (operacion.equals("/"))
+		{
+	 
+			if (valor2 == 0)
+			{
+				JOptionPane.showMessageDialog(null, "No se puede dividir por cero", "Error", JOptionPane.ERROR_MESSAGE);
+				operacion = "null";
+				valor1 = 0;
+				modo = MODE_ENTRADA;
+				inicializa_resultado = true;
+			}
+			else
+				resultado = valor1 / valor2;
+		}	
+	 
+		if (operacion.equals("*"))
+			resultado = valor1 * valor2;
+	 
+		if (operacion.equals("-"))
+			resultado = valor1 - valor2;
+	 
+		if (operacion.equals("+"))
+			resultado = valor1 + valor2;
+	 
+		return resultado;
+	}
+	 
+	public void muestraResultado(int resultado){
+		setResultatString(Integer.toString(resultado));
+		valor1 = resultado;
+		modo = MODE_RESULTADO;
+		inicializa_resultado = true;
+	}
+	 
+	public static void main(String args[]) {
+		Calculadora calculadora = new Calculadora(true);
+	}
+ 
+}
